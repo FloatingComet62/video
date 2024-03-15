2cb03a8e7db0a03fc9bdcea56138cf6961026121
FloatingComet62 <shubh2007rai@gmail.com>
2024-03-15 21:57:18 +0530

    Multiple Equations align up!

--- equation_renderer.py
+++ equation_renderer.py
@@ -5,56 +5,90 @@ options = ['-D', '240', '-bg', 'Transparent', '-fg', 'rgb 1 1 1']
 renderer_already_created = False
 
 
+class CometException(Exception):
+    def __init__(self, msg):
+        Exception.__init__(self, msg)
+
+
 def figure_out_resolution(num_of_equations):
     try:
         if num_of_equations <= 0:
             raise IndexError
         return ['240', '200', '200'][num_of_equations - 1]
     except IndexError as _:
-        return '100'
+        return '180'
 
 
 class EquationRenderer:
+    is_pre_renderer = False
+    padding = 20
+    title_preserve = 80
+
     def __init__(self, window, equations):
         global renderer_already_created
         global options
 
         if renderer_already_created:
-            print("EquationRenderer will cause name conflicts")
-            raise Exception
+            raise CometException("EquationRenderer will cause name conflicts")
         renderer_already_created = True
 
         self.batch = graphics.Batch()
         self.sprites = []
-        self.padding = 20
+        self.window = window
         images = []
 
         options[1] = figure_out_resolution(len(equations))
 
-        for i, equation in enumerate(equations):
-            preview(
-                f"$${equation}$$",
-                viewer='file',
-                filename=f"equations/eqn_{i}.png",
-                dvioptions=options
+        if not self.is_pre_renderer:
+            for i, equation in enumerate(equations):
+                preview(
+                    f"$${equation}$$",
+                    viewer='file',
+                    filename=f"equations/eqn_{i}.png",
+                    dvioptions=options
+                )
+        try:
+            for i in range(len(equations)):
+                images.append(resource.image(f"equations/eqn_{i}.png"))
+        except resource.ResourceNotFoundException:
+            raise CometException(
+                "Some Equation Renders are missing, please set the property \"is_pre_renderer\" to \"False\""
             )
-        for i in range(len(equations)):
-            images.append(resource.image(f"equations/eqn_{i}.png"))
-        images.reverse()
 
         total_height = 0
         for image in images:
-            total_height += image.height + self.padding
+            total_height += image.height
+        total_height += self.padding * (len(images) - 1)
+        start_height = window.height // 2 + total_height // 2
+        current_height = min(start_height, window.height - self.title_preserve)
 
-        current_height = 0
         for image in images:
             self.sprites.append(sprite.Sprite(
                 image,
                 window.width // 2 - image.width // 2,
-                window.height // 2 - total_height // 2 + current_height,
+                current_height - image.height,
                 batch=self.batch
             ))
-            current_height += image.height + self.padding
+            current_height -= image.height + self.padding
 
     def draw(self):
         self.batch.draw()
+
+    def pop_first_eqn(self):
+        if len(self.sprites) == 0:
+            return
+        self.sprites = self.sprites[1:]
+
+        total_height = 0
+        for s in self.sprites:
+            total_height += s.height
+        total_height += self.padding * (len(self.sprites) - 1)
+        start_height = self.window.height // 2 + total_height // 2
+        current_height = min(start_height, self.window.height - self.title_preserve)
+
+        for s in self.sprites:
+            s.update(
+                self.window.width // 2 - s.width // 2,
+                current_height - s.height,
+            )
+            current_height -= s.height + self.padding
--- main.py
+++ main.py
@@ -6,13 +6,26 @@ BUILD = False
 LAST_FRAME = None
 
 window = window.Window()
+EquationRenderer.is_pre_renderer = True
+EquationRenderer.padding = 20
+EquationRenderer.title_preserve = 80
 er = EquationRenderer(window, [
     "y=mx+c",
     "y-c=mx",
-    "\\frac{y-c}{m}=x"
+    "\\frac{y-c}{m}=x",
+    "\\frac{y-c_1}{m}=x",
+    "y=mx+c",
+    "y-c=mx",
+    "\\frac{y-c}{m}=x",
+    "\\frac{y-c_1}{m}=x",
+    "y=mx+c",
+    "y-c=mx",
+    "\\frac{y-c}{m}=x",
+    "\\frac{y-c_1}{m}=x",
 ])
 gray_scale = 50/255
 gl.glClearColor(gray_scale, gray_scale, gray_scale, 1)
+s = shapes.Rectangle(window.width//2 - 1, window.height//2 - 1, 2, 2, (255, 0, 0, 255))
 i = 0
 
 
@@ -25,8 +38,11 @@ def on_draw():
     window.clear()
 
     # Render after this
+    if i and i % 100 == 0:
+        er.pop_first_eqn()
     i += 1
     er.draw()
+    s.draw()
     if BUILD:
         image.get_buffer_manager().get_color_buffer().save(f"frames/{i:>06}.png")
 

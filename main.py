624bf8795f9896ffe59a269c207a064cbfe95561
FloatingComet62 <shubh2007rai@gmail.com>
2024-03-15 22:33:22 +0530

    Jetbrains mono font

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ LAST_FRAME = None
 window = window.Window()
 EquationRenderer.is_pre_renderer = True
 EquationRenderer.padding = 20
-EquationRenderer.title_preserve = 80
+EquationRenderer.title_preserve = 120
 er = EquationRenderer(window, [
     "y=mx+c",
     "y-c=mx",
@@ -25,7 +25,20 @@ er = EquationRenderer(window, [
 ])
 gray_scale = 50/255
 gl.glClearColor(gray_scale, gray_scale, gray_scale, 1)
+
 s = shapes.Rectangle(window.width//2 - 1, window.height//2 - 1, 2, 2, (255, 0, 0, 255))
+
+font.add_file('JetBrainsMonoNL.ttf')
+title = text.Label(
+    'Title Text',
+    "JetBrains Mono",
+    50,
+    x=window.width // 2,
+    y=window.height - 40,
+    anchor_x='center',
+    anchor_y='center'
+)
+
 i = 0
 
 
@@ -43,6 +56,7 @@ def on_draw():
     i += 1
     er.draw()
     s.draw()
+    title.draw()
     if BUILD:
         image.get_buffer_manager().get_color_buffer().save(f"frames/{i:>06}.png")
 
